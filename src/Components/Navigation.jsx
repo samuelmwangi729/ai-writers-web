480e937a42e196c652e651a6ffa9f6d6cd7f0c64
Samuel Mwangi <42680795+samuelmwangi729@users.noreply.github.com>
2023-08-20 06:56:00 +0300

    Navigation Bar redone. Fully responsive

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,41 +1,28 @@
 import React, { Component } from 'react'
 import Logo from '../Assets/logo.png'
-import {TfiSearch}  from 'react-icons/tfi'
 import {AiOutlineMenu}  from 'react-icons/ai'
 class Navigation extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-             
+             nav:false
         }
+        this.setVisible = this.setVisible.bind(this)
     }
-
+    setVisible = ()=>{
+        this.setState({nav:!this.state.nav})
+    }
     render() {
         return (
-            <div className='navigation w-[100%] h-[60px] md:flex items-center justify-between px-2 md:bg-black lg:bg-green-500 xl:bg-gray-700 relative'>
-                <div className="brand pr-5">
+            <div className='navigation flex justify-between px-1 items-center transition-all duration-150 relative'>
+                <div className="brand">
                     <a href='#'>
-                        <img src={Logo} className='lg:w-[250px] md:w-[200px]'/>
+                        <img src={Logo} className='w-[200px] md:w-[250px]'/>
                     </a>
                 </div>
-                <div className="search">
-                    <form>
-                        <div className="InputGroup flex items-center">
-                            <select className='border-l-2 px-2 border-t-2 border-b-2 h-[30px] xl:w-[130px] lg:w-[130px] md:w-[100px] rounded-tl-3xl rounded-bl-3xl focus:outline-none'>
-                                <option label="--select field" className='optionText'></option>
-                            </select> 
-                            <input type="text" placeholder='Search Here...' className='xl:w-[250px] lg:w-[300px] md:w-[200px] border-l-2 px-2 border-t-2 border-b-2 h-[30px] focus:outline-none' />
-                            <button className='w-[50px] border-r-2 px-2 border-t-2 border-b-2 h-[30px] rounded-tr-3xl rounded-br-3xl'>
-                                <span className='font-bold'>
-                                    <TfiSearch size={20} color='orange' className='font-bold'/>
-                                </span>
-                            </button>
-                        </div>
-                    </form>
-                </div>
-                <div className="links pr-3 lg:hidden xl:flex md:hidden hidden">
-                   <ul className='flex items-center pr-2'>
+                <div className="links hidden md:hidden lg:flex">
+                   <ul className='flex items-center justify-between gap-1'>
                     <li>
                         <a href="#">About</a>
                     </li>
@@ -46,19 +33,54 @@ class Navigation extends Component {
                         <a href="#">Pricing</a>
                     </li>
                     <li>
+                        <a href="#">Portfolio</a>
+                    </li>
+                    <li>
                         <a href="#">FAQ</a>
                     </li>
                     <li>
                         <a href="#">Blog</a>
                     </li>
+                    <li>
+                        <a href="#">How It Works</a>
+                    </li>
                    </ul>
                 </div>
-                <div className="buttons whitespace-nowrap">
-                    <button className='bg-orange-600 px-2 py-1 mr-3 text-white'>Sign Up</button>
-                    <button className='bg-[var(--primary-color)] px-2 py-1 mr-3 text-white'>Sign In</button>
+                <div className="buttons transition-all duration-200 flex">
+                    <button className='bg-[var(--primary-color)] text-white px-4 mr-2 whitespace-nowrap'>Sign In</button>
+                    <button className='bg-orange-500 text-white px-4 whitespace-nowrap'>Sign Up</button>
                 </div>
-                <div className="mobileMenu hidden lg:block md:block text-white">
+                <div className="mobileMenu text-black block lg:hidden" onClick={this.setVisible}>
                     <AiOutlineMenu size={30}/>
+                </div>
+                {/* overloay */}
+                <div className={this.state.nav?'overlay bg-black/80 w-full h-screen top-[100%] left-0 absolute z-100 duration-300':'overlay bg-black/60 w-full h-screen top-0 right-[-100%] absolute z-100'}>
+                    <div className={this.state.nav ? 'absolute mobileLinks absolute z-1001 top-[0] right-2 h-[fit-content] bg-[var(--primary-color)] transition duration-3000': 'hidden'}>
+                    <ul className='flex items-left justify-between flex-col text-white'>
+                        <li>
+                            <a href="#">About</a>
+                        
+                        </li>
+                        <li>
+                            <a href="#">Services</a>
+                        </li>
+                        <li>
+                            <a href="#">Pricing</a>
+                        </li>
+                        <li>
+                            <a href="#">Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="#">FAQ</a>
+                        </li>
+                        <li>
+                            <a href="#">Blog</a>
+                        </li>
+                        <li>
+                            <a href="#">How It Works</a>
+                        </li>
+                    </ul>
+                    </div>
                 </div>
             </div>
         )
