1bd8ebe2ac5dc4e78d39bf5eeb594b958c5c59ba
Samuel Mwangi <42680795+samuelmwangi729@users.noreply.github.com>
2023-08-20 07:14:09 +0300

    Close Button Added Up

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Logo from '../Assets/logo.png'
-import {AiOutlineMenu}  from 'react-icons/ai'
+import {AiOutlineMenu,AiOutlineClose}  from 'react-icons/ai'
 class Navigation extends Component {
 
     constructor(props) {
@@ -9,13 +9,19 @@ class Navigation extends Component {
              nav:false
         }
         this.setVisible = this.setVisible.bind(this)
+        this.closeMenu = this.closeMenu.bind(this)
     }
     setVisible = ()=>{
         this.setState({nav:!this.state.nav})
     }
+    closeMenu = () =>{
+        this.setState({
+            nav:!this.state.nav
+        })
+    }
     render() {
         return (
-            <div className='navigation flex justify-between px-1 items-center transition-all duration-150 relative'>
+            <div className='navigation flex justify-between px-1 items-center transition-all duration-150 relative fixed'>
                 <div className="brand">
                     <a href='#'>
                         <img src={Logo} className='w-[200px] md:w-[250px]'/>
@@ -50,12 +56,13 @@ class Navigation extends Component {
                     <button className='bg-[var(--primary-color)] text-white px-4 mr-2 whitespace-nowrap'>Sign In</button>
                     <button className='bg-orange-500 text-white px-4 whitespace-nowrap'>Sign Up</button>
                 </div>
-                <div className="mobileMenu text-black block lg:hidden" onClick={this.setVisible}>
-                    <AiOutlineMenu size={30}/>
+                <div className="mobileMenu text-black block lg:hidden">
+                    {this.state.nav? <AiOutlineClose size={30} onClick={this.closeMenu}/>:<AiOutlineMenu size={30} onClick={this.setVisible}/> }
+                     
                 </div>
                 {/* overloay */}
-                <div className={this.state.nav?'overlay bg-black/80 w-full h-screen top-[100%] left-0 absolute z-100 duration-300':'overlay bg-black/60 w-full h-screen top-0 right-[-100%] absolute z-100'}>
-                    <div className={this.state.nav ? 'absolute mobileLinks absolute z-1001 top-[0] right-2 h-[fit-content] bg-[var(--primary-color)] transition duration-3000': 'hidden'}>
+                <div className={this.state.nav?'lg:hidden overlay bg-black/80 w-full h-screen top-[100%] left-0 absolute z-100 duration-300':'duration-300 overlay bg-black/60 w-full h-screen top-0 right-[-100%] absolute z-100'}>
+                    <div className={this.state.nav ? 'lg:hidden absolute mobileLinks absolute z-1001 top-[0] right-2 h-[fit-content] bg-[var(--primary-color)] transition duration-300': 'hidden'}>
                     <ul className='flex items-left justify-between flex-col text-white'>
                         <li>
                             <a href="#">About</a>
