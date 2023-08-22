dd1fecb51d6e3017b32887c3a35523b76ec39795
Samuel Mwangi <42680795+samuelmwangi729@users.noreply.github.com>
2023-08-22 18:48:53 +0300

    created a footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,16 +11,103 @@ class Footer extends Component {
 
     render() {
         return (
-            <div className='footer bg-black/80 py-2 text-white'>
-                <ul className="footerLinks grid grid-rows-3 px-3 items-center text-center md:flex md:justify-around md:items-center">
+            <div className='footer bg-black/80 py-2 px-4 text-white'>
+                <ul className="footerLinks grid grid-rows-3 px-3 items-center  md:flex md:justify-around md:items-center">
                     <li>
-                        <h1 className='text-xl'>Item 1</h1>
+                        <h1 className='text-xl'>Menu</h1>
+                        <div className="footerLinks">
+                            <ul className="footer">
+                                <li className='w-fulls'>
+                                    <a href="http://">
+                                        About
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://">
+                                        Services
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://">
+                                        Pricing
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://">
+                                        FAQ
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://">
+                                        Blog
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </li>
                     <li>
                         <h1 className='text-xl'>Item 1</h1>
+                        <div className="footerLinks">
+                            <ul className="footer">
+                                <li className='w-fulls'>
+                                    <a href="http://">
+                                        About
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://">
+                                        Services
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://">
+                                        Pricing
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://">
+                                        FAQ
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://">
+                                        Blog
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </li>
                     <li>
                     <h1 className='text-xl'>Item 1</h1>
+                    <div className="footerLinks">
+                            <ul className="footer">
+                                <li className='w-fulls'>
+                                    <a href="http://">
+                                        About
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://">
+                                        Services
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://">
+                                        Pricing
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://">
+                                        FAQ
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://">
+                                        Blog
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </li>
                 </ul>
                 <hr/>
